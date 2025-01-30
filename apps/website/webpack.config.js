1c7c08e78381b433febeb31c816456d2c8353e13
suryansh-yc <suryansh@yosemitecrew.com>
2025-01-30 16:01:49 +0530

    fix: Fixed env variables in website

--- apps/website/webpack.config.js
+++ apps/website/webpack.config.js
@@ -1,6 +1,9 @@
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { NxReactWebpackPlugin } = require('@nx/react/webpack-plugin');
 const { join } = require('path');
+const webpack = require('webpack')
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
   output: {
@@ -31,6 +34,9 @@ module.exports = {
       // Uncomment this line if you don't want to use SVGR
       // See: https://react-svgr.com/
       // svgr: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NX_PUBLIC_VITE_BASE_URL': JSON.stringify(process.env.NX_PUBLIC_VITE_BASE_URL),
     }),
   ],
 };

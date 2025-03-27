b706239e2e4438691da6603857c051e6ae5b0ef5
suryansh-yc <suryansh@yosemitecrew.com>
2025-03-27 21:04:04 +0530

    feat: Added Docker for dev and prod

--- apps/mobileapp/src/app/constants/index.js
+++ apps/mobileapp/src/app/constants/index.js
@@ -1,8 +1,6 @@
-***REMOVED***
-
-***REMOVED***
-
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+export const API_BASE_URL = '';
+export const GOOGLE_API_KEY = '';
+export const AWS_S3_KEY = '';
+export const AWS_S3_SECRET = '';
+export const AWS_S3_REGION = '';
+export const AWS_S3_BUCKET = '';
